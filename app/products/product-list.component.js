65309db16b59e8a83b6eeb872b47b26a113930e3
Mike Ulvila <mike.ulvila@premisehealth.com>
2016-12-28 16:23:42 -0600

    add OnInit interface

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -42,6 +42,9 @@ var ProductListComponent = (function () {
     ProductListComponent.prototype.toggleImage = function () {
         this.showImage = !this.showImage;
     };
+    ProductListComponent.prototype.ngOnInit = function () {
+        console.log('In OnInit');
+    };
     return ProductListComponent;
 }());
 ProductListComponent = __decorate([
